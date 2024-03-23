4809b68b4819d205d1cae8ac26eca7139d58416c
joeldushouyu <joeldushouyu@gmail.com>
2024-03-23 12:36:51 -0400

    script test for 512 * 4

--- writeBackup.c
+++ writeBackup.c
@@ -7,7 +7,7 @@
 #define VENDOR_ID 0x04b4    // Replace with the actual vendor ID of your USB device
 #define PRODUCT_ID 0x00f1   // Replace with the actual product ID of your USB device
 #define ENDPOINT_OUT 0x01   // Replace with the actual OUT endpoint address for bulk transfer
-#define DATA_SIZE  2048*1  // 16384 *100  // 1024*4*4*2// 1024*4 *4   *2// Define the size of the data buffer
+#define DATA_SIZE 16384 * 1 // 16384 *100  // 1024*4*4*2// 1024*4 *4   *2// Define the size of the data buffer
 
 libusb_device_handle *handle = NULL;
 
@@ -24,6 +24,12 @@ void sigint_handler(int signum)
     libusb_exit(NULL);
 
     exit(signum);
+}
+void cb(struct libusb_transfer *transfer)
+{   printf("Here\n");
+    int *completed = transfer->user_data;
+    *completed = 1;
+    printf("complete tranfer\n");
 }
 
 int main()
@@ -60,68 +66,53 @@ int main()
     // Set up signal handler for SIGINT (Ctrl+C)
     signal(SIGINT, sigint_handler);
 
-    // Data to send
+    // create a libusb_transfer
+    struct libusb_transfer *bulkTransfer;
     unsigned char data[DATA_SIZE];
+
+    // Data to send
+
     // Fill data buffer with desired data
     // For example:
-    unsigned char counter = 0;
-    unsigned char dataOut = 0;
-    struct timespec res;
-    res.tv_sec = 0;
-    res.tv_nsec = 86805;
+    unsigned int counter = 0;
+    int completed = 1;
+    bulkTransfer = libusb_alloc_transfer(0);
 
     while (1)
     {
-        res.tv_sec = 0;
-        res.tv_nsec = 868050;
-        char f = 0;
-       dataOut = counter;
-        for (int i = 0; i < DATA_SIZE; i ++)
+        while(completed == 0){
+            // spin to wait after completed
+            libusb_handle_events(NULL);
+        }
+        completed = 0;
+        for (int i = 0; i < DATA_SIZE; i++)
         {
-            // unsigned int *p = (unsigned int *)(data + i);
-            // *p = counter + i;
-            // if(i%(512*4) ==0){
-            //     dataOut++;
-            // }
-            data[i] = dataOut;
-            dataOut++;
-            
-            //  if (i < 1024 * 4 * 4)
-            //  {
-            //      data[i] = counter; // indicate first page
-            //  }
-            //  else
-            //  {
-            //      data[i] = counter;
-            //  }
-            //  if (f == 0)
-            //  {
-            //      data[i] = 0x0;
-            //      f = 1;
-            //  }
-            //  else{
-            //      data[i] = 0x0;
-            //      f = 0;
-            //  }
+            data[i] = counter;
 
-            // data[i] = 0xff;
         }
 
         // Send data
         int actual_sent;
-        r = libusb_bulk_transfer(handle, ENDPOINT_OUT, data, DATA_SIZE, &actual_sent, 1000);
+        printf("About to transfer data\n");
+        //TODO: different stream id?
+        // TODO: exit at counter==9?
+        uint32_t transfer_id = libusb_transfer_get_stream_id(bulkTransfer);
+        printf("Transfer stream id is %d\n", transfer_id);
+        libusb_fill_bulk_stream_transfer(bulkTransfer, handle, ENDPOINT_OUT, transfer_id, data, DATA_SIZE,cb, &completed,1000 );
+        r = libusb_submit_transfer(bulkTransfer);
+         printf("Starting to transfer data at counter = %d\n",counter);
+        // r = libusb_bulk_transfer(handle, ENDPOINT_OUT, data, DATA_SIZE, &actual_sent, 1000);
         if (r == 0)
         {
             printf("Data sent successfully, actual sent is %d\n", actual_sent);
             counter++;
-            if(counter==4){break;}
         }
         else
         {
             fprintf(stderr, "Error sending data: %s\n", libusb_error_name(r));
+     
         }
 
-        clock_nanosleep(CLOCK_MONOTONIC, 0, &res, NULL);
     }
 
     // We never reach this point because of the infinite loop, Ctrl+C will trigger the signal handler
