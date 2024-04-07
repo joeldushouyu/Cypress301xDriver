26adbf54925ff214782a750d36cf7ebcb8acb444
joeldushouyu <joeldushouyu@gmail.com>
2024-04-07 08:20:53 -0400

    separate to array

--- WriteToUSBAsync.c
+++ WriteToUSBAsync.c
@@ -1,24 +1,30 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <libusb-1.0/libusb.h>
-#include <signal.h>
-#include <time.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <libusb-1.0/libusb.h>
+// #include <signal.h>
+// #include <time.h>
 
-#define VENDOR_ID 0x04b4     // Replace with the actual vendor ID of your USB device
-#define PRODUCT_ID 0x00f1    // Replace with the actual product ID of your USB device
-#define ENDPOINT_OUT 0x01    // Replace with the actual OUT endpoint address for bulk transfer
-#define DATA_SIZE 4 * (4096) //   4*4096 // 16384 *100  // 1024*4*4*2// 1024*4 *4   *2// Define the size of the data buffer
+// #define VENDOR_ID 0x04b4     // Replace with the actual vendor ID of your USB device
+// #define PRODUCT_ID 0x00f1    // Replace with the actual product ID of your USB device
+// #define ENDPOINT_OUT 0x01    // Replace with the actual OUT endpoint address for bulk transfer
+// #define CYPRESS_BULK_SIZE 4 * (4096) //   4*4096 // 16384 *100  // 1024*4*4*2// 1024*4 *4   *2// Define the size of the data buffer
 
-#define TOTAL_DATA_ARRAY (480*640)/4096
+// #define TOTAL_BULK_ARRAY (480 * 640) / 4096
+
+#include "AsyncUSB.h"
+
 libusb_device_handle *handle = NULL;
+struct libusb_context *usb_context = NULL;
 
+unsigned int counter = 0;
 
-unsigned int counter= 0;
+// two buffer of the image
+// 1 pointer to the current image
+// 1 lock on the current image
 
 static void read_callback(struct libusb_transfer *transfer)
 {
 
-    int res;
     if (transfer->status == LIBUSB_TRANSFER_COMPLETED)
     {
         // success
@@ -29,94 +35,113 @@ static void read_callback(struct libusb_transfer *transfer)
     }
 
     // resubmit the transfer object back to queue
-    res = libusb_submit_transfer(transfer);
+    libusb_submit_transfer(transfer);
 }
 
-void sigint_handler(int signum)
-{
+// static struct libusb_transfer *create_transfer(libusb_device_handle *handle, size_t length)
+// {
+//     struct libusb_transfer *transfer;
+//     unsigned char *buf;
 
-    // TODO: release the async resources also
-    if (handle != NULL)
-    {
-        // Release the interface and close the device
-        libusb_release_interface(handle, 0);
-        libusb_close(handle);
-    }
+//     // setup the transfer object
+//     buf = malloc(length);
+//     transfer = libusb_alloc_transfer(0); // Zero since is a bulk transfer
+//     // TODO: user data pass back to callback function
 
-    // Cleanup libusb
-    libusb_exit(NULL);
+//     // TODO: testing only for data initialization
+//     unsigned char dataOut = counter;
+//     // for(unsigned int i = 0; i < length; i++){
+//     //     buf[i] = dataOut;
+//     //     dataOut ++;
+//     // }
+//     // counter++;
+//     unsigned int dataToSend;
+//     for (unsigned int i = 0; i < length; i += 4)
+//     {
+//         if (counter < 50)
+//         {
+//             dataToSend = RGBto16BIT(255, 255, 0);
+//         }
+//         else
+//         {
+//             dataToSend = RGBto16BIT(255, 0, 255);
+//         }
 
-    exit(signum);
-}
+//         // neglect top t
+//         buf[i + 2] = (dataToSend >> 8) & 0xFF;
+//         buf[i + 3] = (dataToSend) & 0xFF;
+//     }
+//     if (counter == 74)
+//     {
+//         counter = 0;
+//     }
+//     else
+//     {
+//         counter++;
+//     }
 
-static struct libusb_transfer *create_transfer(libusb_device_handle *handle, size_t length)
-{
-    struct libusb_transfer *transfer;
-    unsigned char *buf;
-
-    // setup the transfer object
-    buf = malloc(length);
-    transfer = libusb_alloc_transfer(0); // Zero since is a bulk transfer
-    // TODO: user data pass back to callback function
-
-    //TODO: testing only for data initialization
-    unsigned char dataOut = counter;
-    for(unsigned int i = 0; i < length; i++){
-        buf[i] = dataOut;
-        dataOut ++;
-    }
-    counter++;
-    
-
-    libusb_fill_bulk_transfer(transfer, handle, ENDPOINT_OUT, buf, length, read_callback, NULL, 5000);
-    return transfer;
-}
-
-
+//     libusb_fill_bulk_transfer(transfer, handle, ENDPOINT_OUT, buf, length, read_callback, NULL, 5000);
+//     return transfer;
+// }
 
 int main()
 {
-    int r;
-    // The context
-    struct libusb_context *usb_context;
-    // Initialize libusb
-    r = libusb_init(&usb_context);
-    if (r < 0)
+    if (initializeUSBDevice(&handle, &usb_context) == -1)
     {
-        fprintf(stderr, "Error initializing libusb: %s\n", libusb_error_name(r));
+        printf("Error in Initialize the USB device\n");
         return 1;
     }
 
-    // Open the device
-    handle = libusb_open_device_with_vid_pid(NULL, VENDOR_ID, PRODUCT_ID);
-    if (!handle)
+    // // create the transfer
+    // for (unsigned int i = 0; i < TOTAL_BULK_ARRAY; i++)
+    // {
+    //     struct libusb_transfer *transfer = create_transfer(handle, CYPRESS_BULK_SIZE);
+    //     libusb_submit_transfer(transfer); // start the intitial transfer
+    // }
+
+    // initalize a frame
+
+    // now, create a list of transfers to point to them
+    USBImageTransfer bulkTransferList;
+
+    for (unsigned int i = 0; i < TOTAL_BULK_ARRAY; i++)
     {
-        fprintf(stderr, "Failed to open device\n");
-        libusb_exit(NULL);
-        return 1;
+        bulkTransferList.transfer_buf[i] = calloc(CYPRESS_BULK_SIZE, 1);
+        bulkTransferList.transfers[i] = libusb_alloc_transfer(0);
     }
 
-    // Claim the interface
-    r = libusb_claim_interface(handle, 0);
-    if (r < 0)
+    // initialize the image
+    unsigned long long  imageCharSize = (CYPRESS_BULK_SIZE) *  (TOTAL_BULK_ARRAY);
+    unsigned long long transferIndex;
+    unsigned long long transferBufIndex;
+    unsigned long long pixel_count = 0;
+    for (unsigned long long  i = 0; i < imageCharSize; i += 4,pixel_count++)
     {
-        fprintf(stderr, "Error claiming interface: %s\n", libusb_error_name(r));
-        libusb_close(handle);
-        libusb_exit(usb_context);
-        return 1;
+        transferIndex = i / (unsigned long long  )(CYPRESS_BULK_SIZE);
+        transferBufIndex = ( i ) % (unsigned long long ) (CYPRESS_BULK_SIZE);
+
+        int dataToSend;
+        
+        if (pixel_count <(640*480)/2)
+        {
+            dataToSend =RGBto16BIT(255, 0, 0);
+        }
+        else
+        {
+            dataToSend = RGBto16BIT(0, 0, 255);
+        }
+
+        unsigned char *buf = bulkTransferList.transfer_buf[transferIndex];
+        buf[transferBufIndex + 2] = (dataToSend >> 8) & 0xFF;
+        buf[transferBufIndex + 3] = (dataToSend ) & 0xFF;
     }
-
-    // Set up signal handler for SIGINT (Ctrl+C)
-    signal(SIGINT, sigint_handler);
-
-
-    // create the transfer
-    for (unsigned int i = 0; i < TOTAL_DATA_ARRAY; i++)
+    for (unsigned int i = 0; i < TOTAL_BULK_ARRAY; i++)
     {
-        struct libusb_transfer *transfer = create_transfer(handle, DATA_SIZE);
-        libusb_submit_transfer(transfer); // start the intitial transfer
-    }
 
+        // start sending
+        libusb_fill_bulk_transfer(bulkTransferList.transfers[i], handle, ENDPOINT_OUT, bulkTransferList.transfer_buf[i], CYPRESS_BULK_SIZE, read_callback, NULL, 5000);
+        libusb_submit_transfer(bulkTransferList.transfers[i]);
+    }
 
     int res;
     while (1)
@@ -124,7 +149,7 @@ int main()
         res = libusb_handle_events(usb_context);
         if (res < 0)
         {
-            printf("handle_events()error # %d\n",res);
+            printf("handle_events()error # %d\n", res);
             /* Break out of this loop only on fatal error.*/
             if (res != LIBUSB_ERROR_BUSY &&
                 res != LIBUSB_ERROR_TIMEOUT &&
